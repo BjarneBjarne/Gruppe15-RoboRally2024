861021ac6e607813a12973de2f68cbdc64533407
Tobias <s235086@student.dtu.dk>
2024-03-20 09:28:35 +0100

    fixed file path

--- RoboRally/src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GameControllerTest.java
+++ RoboRally/src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GameControllerTest.java
@@ -1,9 +1,9 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
-import dk.dtu.compute.se.pisd.roborally.model.Board;
-import dk.dtu.compute.se.pisd.roborally.model.Heading;
-import dk.dtu.compute.se.pisd.roborally.model.Player;
-import dk.dtu.compute.se.pisd.roborally.model.Space;
+import gruppe15.roborally.controller.GameController;
+import gruppe15.roborally.model.Board;
+import gruppe15.roborally.model.Heading;
+import gruppe15.roborally.model.Player;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
