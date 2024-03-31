2ee1a3d80efa73e5ed89c737af51f97e0ae4d598
Tobias <s235086@student.dtu.dk>
2024-03-31 15:27:38 +0200

    added cucumber init

---
+++ RoboRally/src/test/java/dk/dtu/compute/se/pisd/roborally/Gherkin/Stepdefs.java
@@ -0,0 +1,10 @@
+package dk.dtu.compute.se.pisd.roborally.Gherkin;
+
+import io.cucumber.java.en.*;
+
+public class Stepdefs {
+    @Given("test")
+    public void test() {
+        System.out.println("test");
+    }
+}
