32aa5b0f31cf5bcad86c64186fade8b4c276933a
Maximillian Bjørn Mortensen <s236167@dtu.dk>
2024-05-03 14:28:50 +0200

    again card cant move to first register

--- RoboRally/src/main/java/gruppe15/roborally/model/CommandCardField.java
+++ RoboRally/src/main/java/gruppe15/roborally/model/CommandCardField.java
@@ -33,11 +33,21 @@ public class CommandCardField extends Subject {
 
     final public Player player;
 
+    final public int index;
+
     private CommandCard card;
 
     private boolean visible;
 
     public CommandCardField(Player player) {
+        index = 0;
+        this.player = player;
+        this.card = null;
+        this.visible = true;
+    }
+
+    public CommandCardField(Player player, int i) {
+        index = i;
         this.player = player;
         this.card = null;
         this.visible = true;
