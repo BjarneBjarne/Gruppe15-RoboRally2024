0afdc3bac4915f28dd8a30934de7f3e08039106a
Marcus Lemser <s230985@dtu.dk>
2024-05-11 18:10:57 +0200

    Added button effects

--- RoboRally/src/main/java/gruppe15/roborally/view/MainMenuView.java
+++ RoboRally/src/main/java/gruppe15/roborally/view/MainMenuView.java
@@ -11,8 +11,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Effect;
+import javafx.scene.effect.InnerShadow;
+import javafx.scene.effect.Shadow;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class MainMenuView {
     
@@ -26,6 +31,8 @@ public class MainMenuView {
     Button help;
     @FXML
     Button exit;
+
+    Button[] buttons = new Button[4];
     AppController appController;
 
     public AnchorPane getMainMenu() {
@@ -45,6 +52,7 @@ public class MainMenuView {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Effect hover = new InnerShadow(20, Color.STEELBLUE);
         newGame = (Button) mainMenu.lookup("#newGame");
         newGame.setOnAction(e -> appController.newGame());
         loadGame = (Button) mainMenu.lookup("#loadGame");
@@ -53,6 +61,14 @@ public class MainMenuView {
         // help.setOnAction(e -> appController.help());
         exit = (Button) mainMenu.lookup("#exit");
         exit.setOnAction(e -> appController.exit());
+        buttons[0] = newGame;
+        buttons[1] = loadGame;
+        buttons[2] = help;
+        buttons[3] = exit;
+        for (Button button : buttons) {
+            button.setOnMouseEntered(e -> button.setEffect(hover));
+            button.setOnMouseExited(e -> button.setEffect(null));
+        }
         return this;
     }
 }
