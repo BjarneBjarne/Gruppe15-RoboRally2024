124b7d29c64e50a8b9908404f2e1d2fb53874c07
GustavAggeboe <gustavaggeboe@hotmail.com>
2024-05-12 17:18:04 +0200

    Drag and drop

--- RoboRally/src/main/java/gruppe15/roborally/controller/CourseCreatorController.java
+++ RoboRally/src/main/java/gruppe15/roborally/controller/CourseCreatorController.java
@@ -1,13 +1,10 @@
 package gruppe15.roborally.controller;
 
-import gruppe15.observer.Subject;
-import gruppe15.roborally.model.Board;
-import gruppe15.roborally.model.Player;
-import gruppe15.roborally.model.Space;
+import gruppe15.roborally.model.*;
 import gruppe15.roborally.model.boardelements.*;
 import gruppe15.roborally.model.utils.ImageUtils;
+import gruppe15.roborally.view.CardFieldView;
 import gruppe15.roborally.view.SpaceView;
-import gruppe15.roborally.view.ViewObserver;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Bounds;
@@ -17,15 +14,12 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.*;
 import javafx.scene.layout.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static gruppe15.roborally.model.Heading.*;
 import static gruppe15.roborally.model.Phase.INITIALISATION;
 
 public class CourseCreatorController extends VBox {
@@ -40,6 +34,9 @@ public class CourseCreatorController extends VBox {
     private SpaceEventHandler spaceEventHandler;
     private final Image backgroundImage = ImageUtils.getImageFromName("empty.png");
     private final Image backgroundImageStart = ImageUtils.getImageFromName("emptyStart.png");
+    private BoardElements currentBoardElement;
+    private final ImageView selectedBoardElementImageView = new ImageView();
+
 
     public CourseCreatorController() {
         board = new Board(13, 10, 1337);
@@ -66,35 +63,24 @@ public class CourseCreatorController extends VBox {
             }
         }
         CC_boardPane.setOnMouseClicked(spaceEventHandler);
+        CC_boardPane.setOnMouseMoved(spaceEventHandler);
         CC_boardPane.setOnKeyPressed(event -> spaceEventHandler.keyPressed(event));
 
-        // Board elements
-        List<BoardElement> boardElementList = new ArrayList<>();
-        boardElementList.add(new BE_Antenna());
-        boardElementList.add(new BE_BoardLaser(NORTH));
-        boardElementList.add(new BE_ConveyorBelt(NORTH, 1));
-        boardElementList.add(new BE_ConveyorBelt(NORTH, 2));
-        boardElementList.add(new BE_EnergySpace());
-        boardElementList.add(new BE_Gear("Right"));
-        boardElementList.add(new BE_Hole());
-        boardElementList.add(new BE_PushPanel("135", NORTH));
-        boardElementList.add(new BE_PushPanel("24", NORTH));
-        boardElementList.add(new BE_Reboot(NORTH));
-        boardElementList.add(new BE_SpawnPoint(NORTH));
-        for (int i = 1; i <= 6; i++) {
-            boardElementList.add(new BE_Checkpoint(i));
-        }
-
-        for (BoardElement boardElement : boardElementList) {
-            ImageView boardElementImageView = new ImageView(boardElement.getImage());
+        for (BoardElements boardElement : BoardElements.values()) {
+            Image boardElementImage = ImageUtils.getImageFromName(boardElement.imageName);
+            ImageView boardElementImageView = new ImageView(boardElementImage);
             boardElementImageView.setFitWidth(50);
             boardElementImageView.setFitHeight(50);
             Button elementButton = new Button();
             elementButton.setPrefSize(50, 50);
             HBox.setMargin(elementButton, new Insets(25, 12.5, 25, 12.5));
             elementButton.setGraphic(boardElementImageView);
-
             elementButtonsHBox.getChildren().add(elementButton);
+            elementButton.setOnMouseClicked(event -> {
+                currentBoardElement = boardElement;
+                selectedBoardElementImageView.setImage(boardElementImage);
+                selectedBoardElementImageView.startDragAndDrop(TransferMode.MOVE);
+            });
         }
 
     }
@@ -126,20 +112,12 @@ public class CourseCreatorController extends VBox {
             // Object source = event.getSource();
             List<SpaceView> spaceViewsOnMouse = getSpacesAtMouse(event);
             if (spaceViewsOnMouse.isEmpty()) {
-                for (int x = 0; x < board.width; x++) {
-                    for (int y = 0; y < board.height; y++) {
-                        SpaceView spaceView = spaces[x][y];
-                        System.out.println(spaceView.getWidth() + ", " + spaceView.getHeight());
-                    }
-                }
                 return;
             }
             SpaceView spaceView = spaceViewsOnMouse.getFirst();
             //SpaceView spaceView = (SpaceView) source;
             Space space = spaceView.space;
             Board board = space.board;
-
-            System.out.println(space.x + ", " + space.y);
 
             if (space.getBoardElement() instanceof BE_SpawnPoint) {
                 Player currentPlayer = board.getCurrentPlayer();
@@ -156,11 +134,185 @@ public class CourseCreatorController extends VBox {
                     space.setPlayer(board.getPlayer(0));
                 }
             }
+
+           /* if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+
+            }*/
+
+
             event.consume();
         }
 
         public void keyPressed(KeyEvent event) {
             System.out.println("Pressed: " + event.getCode());
         }
+
+
+    }
+
+    enum BoardElements {
+        Antenna("antenna.png"),
+        Laser("laserStart.png"),
+        ConveyorBelt("greenStraight.png"),
+        EnergySpace("energySpace.png"),
+        Gear("gearRight.png"),
+        Hole("hole.png"),
+        PushPanel("push135.png"),
+        Reboot("reboot.png"),
+        SpawnPoint("startField.png");
+        String imageName;
+        BoardElements(String imageName) {
+            this.imageName = imageName;
+        }
+    }
+
+    private class OnDragDetectedHandler implements EventHandler<MouseEvent> {
+
+        @Override
+        public void handle(MouseEvent event) {
+            Object t = event.getTarget();
+            if (t instanceof CardFieldView) {
+                CardFieldView source = (CardFieldView) t;
+                CommandCardField cardField = source.field;
+                if (cardField != null &&
+                        cardField.getCard() != null &&
+                        cardField.player != null &&
+                        cardField.player.board != null &&
+                        cardField.player.board.getPhase().equals(Phase.PROGRAMMING)) {
+                    Dragboard db = source.startDragAndDrop(TransferMode.MOVE);
+                    Image image = source.snapshot(null, null);
+                    db.setDragView(image);
+
+                    ClipboardContent content = new ClipboardContent();
+                    content.put(ROBO_RALLY_CARD, cardFieldRepresentation(cardField));
+
+                    db.setContent(content);
+                    source.setBackground(BG_DRAG);
+                }
+            }
+            event.consume();
+        }
+
+    }
+
+    private class OnDragOverHandler implements EventHandler<DragEvent> {
+
+        @Override
+        public void handle(DragEvent event) {
+            Object t = event.getTarget();
+            if (t instanceof CardFieldView) {
+                CardFieldView target = (CardFieldView) t;
+                CommandCardField cardField = target.field;
+                if (cardField != null &&
+                        (cardField.getCard() == null || event.getGestureSource() == target) &&
+                        cardField.player != null &&
+                        cardField.player.board != null) {
+                    if (event.getDragboard().hasContent(ROBO_RALLY_CARD)) {
+                        event.acceptTransferModes(TransferMode.MOVE);
+                    }
+                }
+            }
+            event.consume();
+        }
+
+    }
+
+    private class OnDragEnteredHandler implements EventHandler<DragEvent> {
+
+        @Override
+        public void handle(DragEvent event) {
+            Object t = event.getTarget();
+            if (t instanceof CardFieldView) {
+                CardFieldView target = (CardFieldView) t;
+                CommandCardField cardField = target.field;
+                if (cardField != null &&
+                        cardField.getCard() == null &&
+                        cardField.player != null &&
+                        cardField.player.board != null) {
+                    if (event.getGestureSource() != target &&
+                            event.getDragboard().hasContent(ROBO_RALLY_CARD)) {
+                        target.setBackground(BG_DROP);
+                    }
+                }
+            }
+            event.consume();
+        }
+
+    }
+
+    private class OnDragExitedHandler implements EventHandler<DragEvent> {
+
+        @Override
+        public void handle(DragEvent event) {
+            Object t = event.getTarget();
+            if (t instanceof CardFieldView) {
+                CardFieldView target = (CardFieldView) t;
+                CommandCardField cardField = target.field;
+                if (cardField != null &&
+                        cardField.getCard() == null &&
+                        cardField.player != null &&
+                        cardField.player.board != null) {
+                    if (event.getGestureSource() != target &&
+                            event.getDragboard().hasContent(ROBO_RALLY_CARD)) {
+                        target.setBackground(BG_DEFAULT);
+                    }
+                }
+            }
+            event.consume();
+        }
+
+    }
+
+    private class OnDragDroppedHandler implements EventHandler<DragEvent> {
+
+        @Override
+        public void handle(DragEvent event) {
+            Object t = event.getTarget();
+            if (t instanceof CardFieldView) {
+                CardFieldView target = (CardFieldView) t;
+                CommandCardField cardField = target.field;
+
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (cardField != null &&
+                        cardField.getCard() == null &&
+                        cardField.player != null &&
+                        cardField.player.board != null) {
+                    if (event.getGestureSource() != target &&
+                            db.hasContent(ROBO_RALLY_CARD)) {
+                        Object object = db.getContent(ROBO_RALLY_CARD);
+                        if (object instanceof String) {
+                            CommandCardField source = cardFieldFromRepresentation((String) object);
+                            if (source != null && gameController.moveCards(source, cardField)) {
+                                // CommandCard card = source.getCard();
+                                // if (card != null) {
+                                // if (gameController.moveCards(source, cardField)) {
+                                // cardField.setCard(card);
+                                success = true;
+                                // }
+                            }
+                        }
+                    }
+                }
+                event.setDropCompleted(success);
+                target.setBackground(BG_DEFAULT);
+            }
+            event.consume();
+        }
+
+    }
+
+    private class OnDragDoneHandler implements EventHandler<DragEvent> {
+
+        @Override
+        public void handle(DragEvent event) {
+            Object t = event.getTarget();
+            if (t instanceof CardFieldView) {
+                CardFieldView source = (CardFieldView) t;
+                source.setBackground(BG_DEFAULT);
+            }
+            event.consume();
+        }
+
     }
 }
