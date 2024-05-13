f2e5c12fa7f878d3dc4fd288d85dc3aadff79f74
Tobias <s235086@student.dtu.dk>
2024-05-13 23:07:02 +0200

    added javadocs

--- RoboRally/src/main/java/gruppe15/roborally/model/boardelements/BE_EnergySpace.java
+++ RoboRally/src/main/java/gruppe15/roborally/model/boardelements/BE_EnergySpace.java
@@ -12,12 +12,14 @@ import java.util.LinkedList;
  * This class represents an energy space on the board and when a player is
  * on an energy space, the player gets an energy cube.
  * The energy space can only give an energy cube once.
- * If the player is on an energy space when the register is 5, the player gets an energy cube.
+ * If the player is on an energy space when the register is 5, the player gets
+ * an energy cube.
  * 
  * @author Tobias Nicolai Frederiksen, s235086@dtu.dk
  */
 public class BE_EnergySpace extends BoardElement {
     private boolean hasEnergyCube = true;
+
     public BE_EnergySpace() {
         super("energySpace.png");
     }
@@ -26,8 +28,20 @@ public class BE_EnergySpace extends BoardElement {
         return hasEnergyCube;
     }
 
+    /**
+     * When a player is on an energy space, the player gets an energy cube.
+     * The energy space can only give an energy cube once.
+     * If the player is on an energy space when the register is 5, the player gets
+     * an energy cube because the player gets an energy cube in the last phase of
+     * the register phase.
+     * 
+     * @param space          the space where the player is located
+     * @param gameController the game controller
+     * @param actionQueue    the queue of actions
+     */
     @Override
-    public boolean doAction(@NotNull Space space, @NotNull GameController gameController, LinkedList<ActionWithDelay> actionQueue) {
+    public boolean doAction(@NotNull Space space, @NotNull GameController gameController,
+            LinkedList<ActionWithDelay> actionQueue) {
         Player player = space.getPlayer();
         if (player != null) {
             if (hasEnergyCube) {
