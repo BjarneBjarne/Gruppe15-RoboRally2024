b701ed44ece07747669abaff2c8f3ddc8d7fac25
Marcus Lemser <s230985@dtu.dk>
2024-05-13 13:00:01 +0200

    First fix

--- RoboRally/src/main/java/gruppe15/roborally/model/boardelements/BE_Gear.java
+++ RoboRally/src/main/java/gruppe15/roborally/model/boardelements/BE_Gear.java
@@ -12,13 +12,13 @@ import java.util.Objects;
 
 public class BE_Gear extends BoardElement {
 
-    private final String direction;
+    private final String rotateDirection;
 
-    public BE_Gear(String direction) {
-        if (!Objects.equals(direction, "Right") && !Objects.equals(direction, "Left"))
-            throw new IllegalArgumentException("Invalid direction: " + direction);
-        this.direction = direction;
-        setImage("gear" + this.direction + ".png");
+    public BE_Gear(String rotateDirection) {
+        if (!Objects.equals(rotateDirection, "Right") && !Objects.equals(rotateDirection, "Left"))
+            throw new IllegalArgumentException("Invalid direction: " + rotateDirection);
+        this.rotateDirection = rotateDirection;
+        setImage("gear" + this.rotateDirection + ".png");
     }
 
     @Override
@@ -27,9 +27,9 @@ public class BE_Gear extends BoardElement {
         if (player == null)
             return false;
         Heading newHeading;
-        if (Objects.equals(this.direction, "Right"))
+        if (Objects.equals(this.rotateDirection, "Right"))
             newHeading = player.getHeading().next();
-        else if (Objects.equals(this.direction, "Left"))
+        else if (Objects.equals(this.rotateDirection, "Left"))
             newHeading = player.getHeading().prev();
         else
             return false;
