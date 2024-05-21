a8fad64ccadc8fa59e64405f9d55a58791aa3823
Maximillian Bjørn Mortensen <s236167@dtu.dk>
2024-05-21 15:11:02 +0200

    damage discard

--- RoboRally/src/main/java/gruppe15/roborally/model/utils/Constants.java
+++ RoboRally/src/main/java/gruppe15/roborally/model/utils/Constants.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Constants {
-    public final static int SPACE_SIZE = 65; // 60; // 75;
-    final public static int CARDFIELD_SIZE = 115;
+    public final static int SPACE_SIZE = 50; // 60; // 75;
+    final public static int CARDFIELD_SIZE = 90;
     public final static List<Integer> PLAYER_NUMBER_OPTIONS = Arrays.asList(2, 3, 4, 5, 6);
 }
