e09b4de7774f100389e18c37d55daf5ef165dd36
GustavAggeboe <gustavaggeboe@hotmail.com>
2024-05-25 16:55:20 +0200

    Fixed integration bugs for courses made with course creator.
    Added debug coordinates.

--- RoboRally/src/main/java/gruppe15/roborally/model/utils/Constants.java
+++ RoboRally/src/main/java/gruppe15/roborally/model/utils/Constants.java
@@ -12,11 +12,16 @@ public class Constants {
     public static double APP_SCALE;
     public static double SPACE_SIZE;
     public static double CARDFIELD_SIZE;
-
     public static void UpdateSizes() {
         SPACE_SIZE = 120 * 0.75;
         CARDFIELD_SIZE = 225 * 0.75;
     }
 
+    // Controls
+    public static double ZOOM_SPEED = 0.1;
+    public static double MIN_ZOOM = 0.2;
+    public static double MAX_ZOOM = 10;
+
+    // Debug
     public final static boolean SHOW_DEBUG_COORDINATES = true;
 }
