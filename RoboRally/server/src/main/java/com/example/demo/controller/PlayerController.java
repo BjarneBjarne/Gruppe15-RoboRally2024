0958879882d7ba0627ddeba4dac35c1f43954acd
Tobias <s235086@student.dtu.dk>
2024-06-13 14:47:26 +0200

    removed unneeded imports and newlines

--- RoboRally/server/src/main/java/com/example/demo/controller/PlayerController.java
+++ RoboRally/server/src/main/java/com/example/demo/controller/PlayerController.java
@@ -2,26 +2,18 @@ package com.example.demo.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.Game;
 import com.example.demo.model.Player;
-import com.example.demo.model.Register;
-import com.example.demo.repository.GameRepository;
 import com.example.demo.repository.PlayerRepository;
-import com.example.demo.repository.RegisterRepository;
 
 @RestController
-//Base endpoint
 @RequestMapping("/players")
-
 public class PlayerController {
 
     PlayerRepository playerRepository;
@@ -51,5 +43,4 @@ public class PlayerController {
         playerRepository.save(playerUpdate);
         return ResponseEntity.ok().build();
     }
-    
 }
--- RoboRally/server/src/main/java/com/example/demo/repository/GameRepository.java
+++ RoboRally/server/src/main/java/com/example/demo/repository/GameRepository.java
@@ -1,10 +1,8 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.model.Game;
 
-public interface GameRepository extends JpaRepository<Game, Long>{
-    
+public interface GameRepository extends JpaRepository<Game, Long> {
 }
