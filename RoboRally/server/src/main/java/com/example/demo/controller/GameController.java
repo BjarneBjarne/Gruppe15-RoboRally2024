2b842c53611767947d1feb6a727f440c68637aba
Marcus Lemser <s230985@dtu.dk>
2024-06-13 17:05:20 +0200

    Moved LobbyController into GameController

--- RoboRally/server/src/main/java/com/example/demo/controller/GameController.java
+++ RoboRally/server/src/main/java/com/example/demo/controller/GameController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +42,35 @@ public class GameController {
         gameRepository.save(game);
 
         return ResponseEntity.ok().body(game.getGameId());
+    }
+
+    @PutMapping(value = "/join-game/{gameId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Long> joinGame(@RequestBody Long playerId, @PathVariable("gameId") Long gameId){
+        
+        Game game = gameRepository.findById(gameId).orElse(null);
+        if (game == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        
+        Player player = playerRepository.findById(playerId).orElse(null);
+        if (player == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        
+        player.setGameId(game.getGameId());
+        playerRepository.save(player);
+
+        game.setNrOfPlayers(game.getNrOfPlayers() + 1);
+        gameRepository.save(game);
+
+        return ResponseEntity.ok().body(gameId);
+
+    }
+
+    @GetMapping(value = "/{gameId}/players", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Player>> getLobby(@PathVariable("gameId") Long gameId){
+        List<Player> players = playerRepository.findAllByGameId(gameId);
+        return ResponseEntity.ok(players);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
