0e52f64789f7c939db064be1137cb3d13fd05184
Tobias <s235086@student.dtu.dk>
2024-06-13 14:24:35 +0200

    restructred progcontroller

--- RoboRally/server/src/main/java/com/example/demo/repository/RegisterRepository.java
+++ RoboRally/server/src/main/java/com/example/demo/repository/RegisterRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.model.Table.Register;
 
 public interface RegisterRepository extends JpaRepository<Register, Long>{
     Register findByPlayerId(Long playerId);
+    Register[] findByGameId(long gameId);
 }
