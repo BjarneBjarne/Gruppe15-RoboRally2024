63bb20c2b60e1df38b1beaa90f8d59f0a73d2bae
Marcus Lemser <s230985@dtu.dk>
2024-06-14 09:44:48 +0200

    Comments upgrade and playerController

--- RoboRally/server/src/main/java/com/example/demo/controller/PlayerController.java
+++ RoboRally/server/src/main/java/com/example/demo/controller/PlayerController.java
@@ -22,6 +22,15 @@ public class PlayerController {
         this.playerRepository = playerRepository;
     }
 
+    /**
+     * Endpoint to create a new player and insert it into the database in 'Players' table
+     * 
+     * @author  Marcus Rémi Lemser Eychenne, s230985
+     * 
+     * @param playerName - the name of the player to be created
+     * 
+     * @return ResponseEntity<Long> - the generated id of the player created
+     */
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Long> createPlayer(@RequestBody String playerName) {
         Player player = new Player();
@@ -30,6 +39,16 @@ public class PlayerController {
         return ResponseEntity.ok(player.getPlayerId());
     }
 
+    /**
+     * Endpoint to update a player in the database in 'Players' table
+     * 
+     * @author  Marcus Rémi Lemser Eychenne, s230985
+     * 
+     * @param player - the player to be updated
+     * @param playerId - the id of the player to be updated
+     * 
+     * @return ResponseEntity<String> - a message indicating the success of the operation
+     */
     @PutMapping(value = "/{playerId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> updatePlayer(@RequestBody Player player, @PathVariable("playerId") Long playerId) {
         Player playerUpdate = playerRepository.findById(playerId).orElse(null);
