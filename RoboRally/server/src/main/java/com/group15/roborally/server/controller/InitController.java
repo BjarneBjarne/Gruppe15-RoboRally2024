02e6c2a83f42882ba4ee813a4d79703c0e4e7d26
Tobias <s235086@student.dtu.dk>
2024-06-17 18:33:02 +0200

    made InitController functional

--- RoboRally/server/src/main/java/com/group15/roborally/server/controller/InitController.java
+++ RoboRally/server/src/main/java/com/group15/roborally/server/controller/InitController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,32 +23,39 @@ public class InitController {
 
     /**
      * Set the spawnpoint of a player
+     * 
+     * @author Tobias Nicolai Frederiksen, s235086@dtu.dk
      * @param playerId
      * @return ResponseEntity with status 200 if successful, 404 if player not found
      */
     @PostMapping(value = "/{playerId}/spawnpoint", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> postSpawnpoint(@PathVariable long playerId/* , @RequestBody Space spawnpoint */) {
-        if (playerRepository.existsById(playerId)) {
+    public ResponseEntity<String> postSpawnpoint(@PathVariable long playerId, @RequestBody int[] spawnpoint) {
+        if (!playerRepository.existsById(playerId)) {
             return ResponseEntity.notFound().build();
         }
         Player player = playerRepository.findById(playerId).get();
-        // player.setSpawnpoint(spawnpoint);
+        if (spawnpoint.length != 2) {
+            return ResponseEntity.status(422).build();
+        }
+        player.setSpawnpoint(spawnpoint);
         playerRepository.save(player);
         return ResponseEntity.ok().build();
     }
 
     /**
      * Set the spawn direction of a player
+     * 
+     * @author Tobias Nicolai Frederiksen, s235086@dtu.dk
      * @param playerId
      * @return ResponseEntity with status 200 if successful, 404 if player not found
      */
     @PostMapping(value = "/{playerId}/spawndirection", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> postSpawndirection(@PathVariable long playerId/* , @RequestBody Heading direction */) {
-        if (playerRepository.existsById(playerId)) {
+    public ResponseEntity<String> postSpawndirection(@PathVariable long playerId, @RequestBody String direction) {
+        if (!playerRepository.existsById(playerId)) {
             return ResponseEntity.notFound().build();
         }
         Player player = playerRepository.findById(playerId).get();
-        // player.setSpawndirection(direction);
+        player.setSpawndirection(direction);
         playerRepository.save(player);
         return ResponseEntity.ok().build();
     }
