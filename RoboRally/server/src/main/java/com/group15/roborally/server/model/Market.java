17997e78e2420369621272654978ff2ae8754ff5
Tobias <s235086@student.dtu.dk>
2024-06-17 14:56:07 +0200

    added market

--- RoboRally/server/src/main/java/com/group15/roborally/server/model/Market.java
+++ RoboRally/server/src/main/java/com/group15/roborally/server/model/Market.java
@@ -1,29 +1,27 @@
-// package com.group15.roborally.server.model;
+package com.group15.roborally.server.model;
 
-// import jakarta.persistence.*;
-// import lombok.AllArgsConstructor;
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-// import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-// @Entity
-// @Table(name = "games")
-// @Getter
-// @Setter
-// @NoArgsConstructor
-// @AllArgsConstructor
+@Entity
+@Table(name = "markets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
-// public class Market {
+public class Market {
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long marketId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long marketId;
 
-//     private Long gameId;
+    private Long gameId;
 
-//     private String[] cards;
+    private String[] cards;
 
-//     private int turn;
-    
-//     @ManyToOne(fetch = FetchType.LAZY)
-// }
+    private int turn;
+}
--- RoboRally/server/src/main/java/com/group15/roborally/server/repository/MarketRepository.java
+++ RoboRally/server/src/main/java/com/group15/roborally/server/repository/MarketRepository.java
@@ -1,9 +1,9 @@
-// package com.group15.roborally.server.repository;
+package com.group15.roborally.server.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// import com.group15.roborally.server.model.Market;
+import com.group15.roborally.server.model.Market;
 
-// public interface MarketRepository extends JpaRepository<Market, Long>{
+public interface MarketRepository extends JpaRepository<Market, Long>{
     
-// }
+}
