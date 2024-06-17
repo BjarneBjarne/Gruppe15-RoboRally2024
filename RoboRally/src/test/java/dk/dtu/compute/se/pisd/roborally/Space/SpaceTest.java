63429ca43dead42946d7f223e545d135a2f681dc
Maximillian Bjørn Mortensen <s236167@dtu.dk>
2024-06-17 15:16:52 +0200

    space test layout

---
+++ RoboRally/src/test/java/dk/dtu/compute/se/pisd/roborally/Space/SpaceTest.java
@@ -0,0 +1,31 @@
+package dk.dtu.compute.se.pisd.roborally.Space;
+
+import org.junit.jupiter.api.Test;
+
+public class SpaceTest {
+
+    @Test
+    void setPlayer(){
+
+    }
+
+    @Test
+    void getDistanceFromOtherSpace(){
+
+    }
+
+    @Test
+    void getIsWallBetween(){
+
+    }
+
+    @Test
+    void getDirectionToOtherSpace(){
+
+    }
+
+    @Test
+    void getSpaceNextTo(){
+
+    }
+}
