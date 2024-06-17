f90a659e133c5cd27b4a1c03241f357d0afa69fe
Tobias <s235086@student.dtu.dk>
2024-06-17 21:20:43 +0200

    completed ProgController and its tests

--- RoboRally/server/src/main/java/com/group15/roborally/server/controller/ProgController.java
+++ RoboRally/server/src/main/java/com/group15/roborally/server/controller/ProgController.java
@@ -1,6 +1,10 @@
 package com.group15.roborally.server.controller;
 
+import com.group15.roborally.server.model.Game;
+import com.group15.roborally.server.model.Player;
 import com.group15.roborally.server.model.Register;
+import com.group15.roborally.server.repository.GameRepository;
+import com.group15.roborally.server.repository.PlayerRepository;
 import com.group15.roborally.server.repository.RegisterRepository;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,34 +14,67 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Set;
-
 @RestController
 public class ProgController {
 
+    private GameRepository gameRepository;
+    private PlayerRepository playerRepository;
     private RegisterRepository registerRepository;
 
-    public ProgController(RegisterRepository registerRepository) {
+    public ProgController(GameRepository gameRepository, PlayerRepository playerRepository, RegisterRepository registerRepository) {
+        this.gameRepository = gameRepository;
+        this.playerRepository = playerRepository;
         this.registerRepository = registerRepository;
     }
 
-    // @GetMapping(value = "/games/{gameId}/registers", consumes = MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<Register[]> getRegisters(@PathVariable("gameId") long gameId) {
-    //     Register[] registers = registerRepository.findByGameId(gameId);
-    //     return ResponseEntity.ok(registers);
-    // }
-
-    @PostMapping(value = "/games/{gameId}/registers/{playerId}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void postRegister(@RequestBody Register register, @PathVariable("playerId") long playerId) {
+    /**
+     * Endpoint to post a register for a player in a game
+     * 
+     * @author Tobias Nicolai Frederiksen, s235086@dtu.dk
+     * 
+     * @param register
+     * @param playerId
+     * @param turn
+     * @return ResponseEntity<String> 
+     */
+    @PostMapping(value = "/players/{playerId}/registers/{turn}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> postRegister(@RequestBody Register register, @PathVariable("playerId") long playerId, @PathVariable("turn") int turn) {
+        Player player = playerRepository.findById(playerId).orElse(null);
+        if (player == null) {
+            return ResponseEntity.status(404).build();
+        }
+        if (playerId <= 0L || turn <= 0 || player.getGameId() != register.getGameId()) {
+            return ResponseEntity.status(422).build();
+        }
+        if (register.getM1().equals(null) 
+            || register.getM2().equals(null) 
+            || register.getM3().equals(null) 
+            || register.getM4().equals(null) 
+            || register.getM5().equals(null)) {
+            return ResponseEntity.status(422).build();
+        }
         registerRepository.save(register);
+        return ResponseEntity.ok().build();
     }
 
-    @PostMapping(value = "/games/{gameId}/choice", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void postChoice() {
-    }
-
-    @GetMapping(value = "/games/{gameId}/choice", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> getRegisters() {
-        return ResponseEntity.badRequest().build();
+    /**
+     * Endpoint to get all registers for a game
+     * 
+     * @author Tobias Nicolai Frederiksen, s235086@dtu.dk
+     * 
+     * @param gameId
+     * @return ResponseEntity<Register[]> 
+     */
+    @GetMapping(value = "/games/{gameId}/registers", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Register[]> getRegisters(@PathVariable("gameId") long gameId) {
+        Game game = gameRepository.findById(gameId).orElse(null);
+        if (game == null) {
+            return ResponseEntity.status(404).build();
+        }
+        if (gameId <= 0L) {
+            return ResponseEntity.status(422).build();
+        }
+        Register[] registers = registerRepository.findByGameId(gameId);
+        return ResponseEntity.ok(registers);
     }
 }
---
+++ RoboRally/server/src/test/java/com/group15/roborally/server/controller/ProgControllerTest.java
@@ -0,0 +1,113 @@
+package com.group15.roborally.server.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.google.gson.Gson;
+import com.group15.roborally.server.model.Register;
+
+// A Spring Boot Test which tests the endpoints in the ProgController
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProgControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final Gson gson;
+
+    public ProgControllerTest() {
+        this.gson = new Gson();
+    }
+
+    /**
+     * Test the postRegister endpoint in the ProgController
+     * 
+     * @author Tobias Nicolai Frederiksen, s235086@dtu.dk
+     * 
+     * @return Register - the register created to be tested
+     * @throws Exception
+     */
+    private Register createGameAndPlayerWithRegister() throws Exception {
+        long expectedGameId = 1L;
+        mockMvc.perform(MockMvcRequestBuilders.get("/games")
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(expectedGameId + ""));
+        long expectedPlayerId = 1L;
+        mockMvc.perform(MockMvcRequestBuilders.post("/games/{gameId}/join", expectedGameId)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content("player1"))
+                .andExpect(status().isOk());
+
+        Register register = new Register();
+        register.setGameId(expectedGameId);
+        register.setPlayerId(expectedPlayerId);
+        register.setRegisterId(1L);
+        register.setTurn(0);
+        register.setM1("card53");
+        register.setM2("card2");
+        register.setM3("card32");
+        register.setM4("card21");
+        register.setM5("card75");
+        register.setPlayer(null);
+
+        return register;
+    }
+
+    /**
+     * Test the postRegister endpoint in the ProgController
+     * 
+     * @author Tobias Nicolai Frederiksen, s235086@dtu.dk
+     * 
+     * @throws Exception
+     */
+    @Test
+    @DirtiesContext
+    public void postRegisterTest() throws Exception {
+        Register register = createGameAndPlayerWithRegister();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/players/1/registers/1")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(gson.toJson(register)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/players/2/registers/1")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(gson.toJson(register)))
+                .andExpect(status().isNotFound());
+    }
+
+    /**
+     * Test the getRegisters endpoint in the ProgController
+     * 
+     * @author Tobias Nicolai Frederiksen, s235086@dtu.dk
+     * 
+     * @throws Exception
+     */
+    @Test
+    @DirtiesContext
+    public void getRegistersTest() throws Exception {
+        Register register = createGameAndPlayerWithRegister();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/players/1/registers/1")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(gson.toJson(register)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/games/1/registers")
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+        // .andExpect(content().string("[" + gson.toJson(register) + "]"))
+        ;
+    }
+}
