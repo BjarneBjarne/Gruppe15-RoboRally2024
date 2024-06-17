2dc3ee02cd7443f74293e9001d801eec13073c63
Maximillian Bjørn Mortensen <s236167@dtu.dk>
2024-06-17 11:02:25 +0200

    attemtToBuyCard under dev

--- RoboRally/src/test/java/dk/dtu/compute/se/pisd/roborally/UpgradeShop/UpgradeShopTest.java
+++ RoboRally/src/test/java/dk/dtu/compute/se/pisd/roborally/UpgradeShop/UpgradeShopTest.java
@@ -1,7 +1,7 @@
 package dk.dtu.compute.se.pisd.roborally.UpgradeShop;
 
-import gruppe15.roborally.model.Board;
-import gruppe15.roborally.model.UpgradeShop;
+import gruppe15.roborally.model.*;
+import gruppe15.roborally.model.upgrade_cards.UpgradeCard;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,37 +9,42 @@ import org.junit.jupiter.api.Test;
 
 public class UpgradeShopTest{
 
-    private UpgradeShop upgradeShop;
-    private Board board;
-
-    @BeforeEach
-    void setup(){
-        Board board = new Board(null, null, "test", 1);
-        upgradeShop = new UpgradeShop(board);
-    }
-
-    @AfterEach
-    void teardown(){
-        upgradeShop = null;
-    }
-
     @Test
     void attemptBuyCardFromShop(){
-        upgradeShop.attemptBuyCardFromShop(null, null);
+        Board board = new Board(null, null, "test", 1);
+        Player p1 = new Player(board, Robots.getRobotByName("SPIN BOT"), "p1");
+        Player p2 = new Player(board, Robots.getRobotByName("ZOOM BOT"), "p2");
+        board.addPlayer(p1);
+        board.addPlayer(p2);
+        UpgradeShop upgradeShop = new UpgradeShop(board);
+        upgradeShop.refillAvailableCards();
+        CardField upgradeCardField = upgradeShop.getAvailableCardsField(1);
+        UpgradeCard upgradeCard = (UpgradeCard) upgradeCardField.getCard();
+        p1.setEnergyCubes(upgradeCard.getPurchaseCost());
+        UpgradeCard result = upgradeShop.attemptBuyCardFromShop(upgradeCardField, p1);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(0, p1.getEnergyCubes());
+        Assertions.assertSame(upgradeCard, result);
     }
 
     @Test
     void attemptReceiveFreeCardFromShop(){
+        Board board = new Board(null, null, "test", 1);
+        UpgradeShop upgradeShop = new UpgradeShop(board);
         upgradeShop.attemptReceiveFreeCardFromShop(null, null);
     }
 
     @Test
     void refillAvailableCards(){
+        Board board = new Board(null, null, "test", 1);
+        UpgradeShop upgradeShop = new UpgradeShop(board);
         upgradeShop.refillAvailableCards();
     }
 
     @Test
     void update(){
+        Board board = new Board(null, null, "test", 1);
+        UpgradeShop upgradeShop = new UpgradeShop(board);
         upgradeShop.update(null);
     }
 }
