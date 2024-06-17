79806a00ba4ee344b517b2741fdd39bba4188234
BjarneBjarne <s235107@dtu.dk>
2024-06-18 00:22:26 +0200

    Added MockitoFrameWork and set up first test in GameControllerTest.java

--- RoboRally/src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GameControllerTest.java
+++ RoboRally/src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GameControllerTest.java
@@ -1,30 +1,124 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
 import gruppe15.roborally.controller.GameController;
-import gruppe15.roborally.coursecreator.CC_CourseData;
-import gruppe15.roborally.coursecreator.CC_JsonUtil;
 import gruppe15.roborally.model.*;
-import javafx.util.Pair;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static gruppe15.roborally.model.Phase.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class GameControllerTest {
+    private Board board;
+    private Runnable gameOverMethod;
+    private GameController gameController;
+    private Player player1;
+    private Player player2;
+    private Queue<Player> mockPriorityList;
+
+    @BeforeEach
+    void setUp() {
+        board = mock(Board.class);
+        gameOverMethod = mock(Runnable.class);
+        player1 = mock(Player.class);
+        player2 = mock(Player.class);
+        mockPriorityList = mock(Queue.class);
+        gameController = new GameController(board, gameOverMethod);
+    }
+
+    @Test
+    public void startProgrammingPhaseTest() {
+
+        AtomicReference<Phase> currentPhase = new AtomicReference<>();
+        doAnswer(invocation -> {
+            currentPhase.set(invocation.getArgument(0));
+            return null;
+        }).when(board).setCurrentPhase(any(Phase.class));
+        when(board.getCurrentPhase()).thenAnswer(invocation -> currentPhase.get());
+
+        when(mockPriorityList.peek()).thenReturn(player1);
+        when(board.getPriorityList()).thenReturn(mockPriorityList);
+
+        gameController.startProgrammingPhase();
+
+        assertEquals(PROGRAMMING, board.getCurrentPhase());
+    }
+
+
+}
+
+
+
+/*    @Test
+    void testExecutePrograms() {
+        gameController.executePrograms();
+
+        verify(board).setStepMode(false);
+        verify(gameController).handlePlayerRegister();
+    }
+}
+
+
+
+    /*    @Test
+    void testStartProgrammingPhase() {
+
+        when(board.getPlayers()).thenReturn(List.of(player1, player2));
+        when(board.getNoOfPlayers()).thenReturn(2);
+
+        gameController.startProgrammingPhase();
+
+        verify(board).setCurrentPhase(PROGRAMMING);
+        verify(board).setCurrentRegister(0);
+        verify(player1).discardAll();
+        verify(player2).discardAll();
+        verify(player1, times(NO_OF_CARDS_IN_HAND)).drawHand();
+        verify(player2, times(NO_OF_CARDS_IN_HAND)).drawHand();
+    }
+
+}
+
+    @Test
+    void testBeginGame() {
+        Player player1 = mock(Player.class);
+        Player player2 = mock(Player.class);
+
+        when(board.getPlayers()).thenReturn(List.of(player1, player2));
+
+        gameController.beginGame();
+
+        verify(player1, times(STARTING_UPGRADE_CARDS.size())).tryAddFreeUpgradeCard(any(UpgradeCard.class), eq(gameController));
+    }
+}*/
+
+/*class GameControllerTest {
 
     private GameController gameController;
 
     @BeforeEach
     void setUp() {
-        List<CC_CourseData> courses = CC_JsonUtil.getCoursesInFolder("courses");
-        CC_CourseData courseData = courses.get(0);
-        Pair<List<Space[][]>, Space[][]> courseSpaces = courseData.getGameSubBoards();
-        Board board = new Board(courseSpaces.getKey(), courseSpaces.getValue(), courseData.getCourseName(), courseData.getNoOfCheckpoints());
-        // gameController = new GameController(board);
+        Robots robot = Robots.getRobotByName("SPIN BOT");
+        Space[][] spaces = new Space[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                spaces[i][j] = new Space(null, i, j, null);
+            }
+        }
+        Board board = new Board(null, spaces, "TestBoard", 4);
+        Runnable gameOverMethod = null;
+        gameController = new GameController(board, gameOverMethod);
+        Player player = new Player(board, robot, "testPlayer");
+        board.addPlayer(player);
+        player.setSpace(board.getSpace(0,0));
+        player.setHeading(Heading.values()[0]);
+        board.setCurrentPlayer(board.getPlayer(0));
+
         for (int i = 0; i < 6; i++) {
-            Player player = new Player(board, null,"Player " + i);
+            Player player = new Player(board, robot,"Player " + i);
             board.addPlayer(player);
             player.setSpace(board.getSpace(i, i));
             player.setHeading(Heading.values()[i % Heading.values().length]);
@@ -41,7 +135,7 @@ class GameControllerTest {
     void moveCurrentPlayerToSpace() {
         Board board = gameController.board;
         Player player1 = board.getPlayer(0);
-        Player player2 = board.getPlayer(1);
+        //Player player2 = board.getPlayer(1);
 
 
 
@@ -49,7 +143,7 @@ class GameControllerTest {
 
         Assertions.assertEquals(player1, board.getSpace(0, 4).getPlayer(), "Player " + player1.getName() + " should beSpace (0,4)!");
         Assertions.assertNull(board.getSpace(0, 0).getPlayer(), "Space (0,0) should be empty!");
-        Assertions.assertEquals(player2, board.getCurrentPlayer(), "Current player should be " + player2.getName() +"!");
+        //Assertions.assertEquals(player2, board.getCurrentPlayer(), "Current player should be " + player2.getName() +"!");
 
     }
 
@@ -60,7 +154,7 @@ class GameControllerTest {
 
         current.queueCommand(Command.MOVE_1, gameController);
 
-        Assertions.assertEquals(current, board.getSpace(0, 1).getPlayer(), "Player " + current.getName() + " should beSpace (0,1)!");
+        Assertions.assertEquals(current, board.getSpace(0, 1).getPlayer(), "Player " + current.getName() + " should be Space (0,1)!");
         Assertions.assertEquals(Heading.SOUTH, current.getHeading(), "Player 0 should be heading SOUTH!");
         Assertions.assertNull(board.getSpace(0, 0).getPlayer(), "Space (0,0) should be empty!");
     }
@@ -99,4 +193,4 @@ class GameControllerTest {
         Assertions.assertEquals(Heading.EAST, current.getHeading(), "Player 0 should be heading EAST!");
     }
 
-}
+}*/
