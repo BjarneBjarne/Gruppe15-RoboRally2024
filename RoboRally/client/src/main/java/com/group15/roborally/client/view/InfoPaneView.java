15e281136734239ce3473c961ba07a0a70af2a10
Tobias <s235086@student.dtu.dk>
2024-06-19 10:09:41 +0200

    moved code from appcontroller to create an InfoPaneView class

---
+++ RoboRally/client/src/main/java/com/group15/roborally/client/view/InfoPaneView.java
@@ -0,0 +1,67 @@
+package com.group15.roborally.client.view;
+
+import com.group15.observer.Subject;
+import com.group15.roborally.client.utils.TextUtils;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.StrokeType;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+
+public class InfoPaneView extends StackPane implements ViewObserver {
+
+    private final Text infoText;
+
+    public InfoPaneView() {
+        // mainPane.getChildren().add(infoPane);
+
+        // difference?
+        StackPane.setAlignment(this, Pos.CENTER);
+        setAlignment(Pos.CENTER);
+
+        setStyle("-fx-background-color: #000000A5");
+
+        infoText = new Text();
+        Font textFont = TextUtils.loadFont("OCRAEXT.TTF", 90);
+        infoText.setFont(textFont);
+        infoText.setFill(Color.WHITE);
+        infoText.setStroke(Color.BLACK);
+        infoText.setStrokeWidth(3);
+        infoText.setStrokeType(StrokeType.OUTSIDE);
+        infoText.setWrappingWidth(2560);
+        infoText.setTextAlignment(TextAlignment.CENTER);
+        StackPane.setMargin(infoText, new Insets(0, 0, 75, 0));
+        infoText.setText("");
+        disable();
+        getChildren().add(infoText);
+    }
+
+    public void setInfoText(String text) {
+        if (text.isEmpty()) {
+            disable();
+            return;
+        }
+        infoText.setText(text);
+        enable();
+    }
+
+    public void enable() {
+        setDisable(false);
+        setVisible(true);
+    }
+
+    public void disable() {
+        setDisable(true);
+        setVisible(false);
+    }
+
+    @Override
+    public void updateView(Subject subject) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateView'");
+    }
+}
