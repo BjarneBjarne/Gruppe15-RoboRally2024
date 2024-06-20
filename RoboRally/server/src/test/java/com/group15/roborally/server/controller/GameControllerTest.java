1d741bd6264228c737bf5e6ff4daabefb3cb7354
Tobias <s235086@student.dtu.dk>
2024-06-20 18:54:16 +0200

    changed test

--- RoboRally/server/src/test/java/com/group15/roborally/server/controller/GameControllerTest.java
+++ RoboRally/server/src/test/java/com/group15/roborally/server/controller/GameControllerTest.java
@@ -12,6 +12,10 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.group15.roborally.server.model.Player;
+
 // A Spring Boot Test which tests the endpoints in the GameController
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,6 +23,12 @@ public class GameControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    private Gson gson;
+
+    public GameControllerTest() {
+        gson = new GsonBuilder().serializeNulls().create();
+    }
 
     /**
      * Test the createGame endpoint in the GameController
@@ -49,26 +59,24 @@ public class GameControllerTest {
     @Test
     @DirtiesContext
     public void joinGameTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/games"));
+        mockMvc.perform(MockMvcRequestBuilders.get("/games"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("1"));
 
-        // Player expectedPlayer = new Player();
-        // expectedPlayer.setPlayerName("player1");
-        // expectedPlayer.setGameId(1L);
-        // expectedPlayer.setPlayerId(1L);
-        // expectedPlayer.setIsReady(0);
-        // String expectedPlayerJson = gson.toJson(expectedPlayer);
+        Player expectedPlayer = new Player(1, 1, null, "player1",
+                null, null, 0, null, null);
         mockMvc.perform(MockMvcRequestBuilders.post("/games/1/join").contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content("player1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-                // .andExpect(content().string(expectedPlayerJson));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                /* .andExpect(content().string(gson.toJson(expectedPlayer))) */;
 
         mockMvc.perform(MockMvcRequestBuilders.post("/games/1/join").contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content("player2"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-                // .andExpect(content().string(
-                //         "{\"playerId\":2,\"gameId\":1,\"robotName\":null,\"playerName\":\"player2\",\"isReady\":0}"));
+        // .andExpect(content().string(
+        // "{\"playerId\":2,\"gameId\":1,\"robotName\":null,\"playerName\":\"player2\",\"isReady\":0}"));
 
         mockMvc.perform(MockMvcRequestBuilders.post("/games/2/join").contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content("player1"))
@@ -101,16 +109,16 @@ public class GameControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/games/1/players"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-                // .andExpect(content().string(
-                //         "[{\"playerId\":1,\"gameId\":1,\"robotName\":null,\"playerName\":\"player1\",\"isReady\":0}]"));
+        // .andExpect(content().string(
+        // "[{\"playerId\":1,\"gameId\":1,\"robotName\":null,\"playerName\":\"player1\",\"isReady\":0}]"));
 
         mockMvc.perform(MockMvcRequestBuilders.post("/games/1/join").contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content("player2")).andExpect(status().isOk());
         mockMvc.perform(MockMvcRequestBuilders.get("/games/1/players"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-                // .andExpect(content().string(
-                //         "[{\"playerId\":1,\"gameId\":1,\"robotName\":null,\"playerName\":\"player1\",\"isReady\":0},{\"playerId\":2,\"gameId\":1,\"robotName\":null,\"playerName\":\"player2\",\"isReady\":0}]"));
+        // .andExpect(content().string(
+        // "[{\"playerId\":1,\"gameId\":1,\"robotName\":null,\"playerName\":\"player1\",\"isReady\":0},{\"playerId\":2,\"gameId\":1,\"robotName\":null,\"playerName\":\"player2\",\"isReady\":0}]"));
 
         mockMvc.perform(MockMvcRequestBuilders.get("/games/2/players"))
                 .andExpect(status().isNotFound());
@@ -131,16 +139,16 @@ public class GameControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/games/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-                // .andExpect(content().string(
-                //         "{\"gameId\":1,\"nrOfPlayers\":0,\"phase\":\"LOBBY\",\"courseName\":null,\"players\":[]}"));
+        // .andExpect(content().string(
+        // "{\"gameId\":1,\"nrOfPlayers\":0,\"phase\":\"LOBBY\",\"courseName\":null,\"players\":[]}"));
 
         mockMvc.perform(MockMvcRequestBuilders.post("/games/1/join").contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content("player1")).andExpect(status().isOk());
         mockMvc.perform(MockMvcRequestBuilders.get("/games/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-                // .content(
-                //         "{\"gameId\":1,\"nrOfPlayers\":1,\"phase\":\"LOBBY\",\"courseName\":null,\"players\":[]}"))
+        // .content(
+        // "{\"gameId\":1,\"nrOfPlayers\":1,\"phase\":\"LOBBY\",\"courseName\":null,\"players\":[]}"))
 
         mockMvc.perform(MockMvcRequestBuilders.get("/games/2"))
                 .andExpect(status().isNotFound());
