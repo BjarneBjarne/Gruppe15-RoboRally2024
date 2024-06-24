6c6f058c309fa2358821fd87e53f97675e9e0945
GustavAggeboe <gustavaggeboe@hotmail.com>
2024-06-24 15:32:37 +0200

    Fixes to upgrade shop and players getting upgrade cards.
    Changes to onActivated in UpgradeCard.

--- roborally/client/src/main/java/com/group15/roborally/client/model/upgrade_cards/UpgradeCardTemporary.java
+++ roborally/client/src/main/java/com/group15/roborally/client/model/upgrade_cards/UpgradeCardTemporary.java
@@ -14,11 +14,6 @@ public abstract class UpgradeCardTemporary extends UpgradeCard {
     }
 
     @Override
-    protected void onActivated() {
-        printUsage();
-    }
-
-    @Override
     public void tryActivate() {
         super.tryActivate();
         // Remove card on use
