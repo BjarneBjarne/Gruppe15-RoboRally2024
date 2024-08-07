4eddb73daf525bcf7c4dc73c00625253f8444ba3
GustavAggeboe <gustavaggeboe@hotmail.com>
2024-08-07 05:59:56 +0200

    Updated incrementing of version.

--- roborally/build_client.ps1
+++ roborally/build_client.ps1
@@ -1,10 +1,10 @@
-# Get the directory of the current script
+# Variables
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $projectRoot = Join-Path $scriptDir "client"
 $outputDir = Join-Path $projectRoot "target\output"
 $jlinkImageDir = Join-Path $projectRoot "target\jlink-image"
 
-# Define the version variable from version file
+# Version
 $versionFile = Join-Path $scriptDir "version"
 if (-not (Test-Path -Path $versionFile)) {
     Write-Output "Version file not found. Please create version file with the version number."
@@ -12,16 +12,27 @@ if (-not (Test-Path -Path $versionFile)) {
 }
 $version = Get-Content -Path $versionFile
 
-Write-Output "`n* Building Windows installer for RoboRally client version $version *`n"
+Write-Output "`n > Building Windows installer for RoboRally client version $version < `n"
 
-# Check if Maven is installed
+# Maven check
 $mvnPath = Get-Command mvn -ErrorAction SilentlyContinue
 if (-not $mvnPath) {
     Write-Output "Maven is not installed. Please install Maven to continue."
     exit 1
 }
+# JAVA_HOME check
+if (-not $env:JAVA_HOME) {
+    Write-Output "JAVA_HOME is not set. Please set JAVA_HOME to the path of your JDK."
+    exit 1
+}
 
-# Run mvn clean package
+# PATH_TO_FX check
+if (-not $env:PATH_TO_FX) {
+    Write-Output "PATH_TO_FX is not set. Please set PATH_TO_FX to the path of your JavaFX SDK."
+    exit 1
+}
+
+# mvn clean package
 Write-Output "Running mvn clean package..."
 $mvnResult = & mvn -f "$scriptDir\pom.xml" clean package
 if ($LASTEXITCODE -ne 0) {
@@ -29,30 +40,17 @@ if ($LASTEXITCODE -ne 0) {
     exit 1
 }
 
-# Clean output and jlink directories if they exist
+# Clean build directories
 if (Test-Path -Path $outputDir) {
     Write-Output "Cleaning output directory..."
     Remove-Item -Recurse -Force $outputDir
 }
-
 if (Test-Path -Path $jlinkImageDir) {
     Write-Output "Cleaning jlink image directory..."
     Remove-Item -Recurse -Force $jlinkImageDir
 }
 
-# Check if JAVA_HOME is set
-if (-not $env:JAVA_HOME) {
-    Write-Output "JAVA_HOME is not set. Please set JAVA_HOME to the path of your JDK."
-    exit 1
-}
-
-# Check if PATH_TO_FX is set
-if (-not $env:PATH_TO_FX) {
-    Write-Output "PATH_TO_FX is not set. Please set PATH_TO_FX to the path of your JavaFX SDK."
-    exit 1
-}
-
-# Create custom runtime image using jlink
+# JLink runtime image
 Write-Output "Making runnable image with jlink..."
 jlink --module-path "$env:JAVA_HOME\jmods;$env:PATH_TO_FX" `
       --add-modules java.base,java.desktop,javafx.controls,javafx.fxml,jdk.zipfs `
@@ -62,7 +60,7 @@ if ($LASTEXITCODE -ne 0) {
     exit 1
 }
 
-# Package the application using jpackage
+# JPackage installer
 Write-Output "Packaging installer with jpackage..."
 
 jpackage `
